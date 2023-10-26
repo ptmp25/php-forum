ef46c36b16fc1bd7c0d2c1ca010ecc9d1244d721
khang <loroqua0@gmail.com>
2023-10-26 15:29:50 +0700

    ui changes

--- member.php
+++ member.php
@@ -1,13 +1,3 @@
-<?php
-session_start();
-
-// Check if the user is not logged in and redirect to login.php
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
-    exit;
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -56,8 +46,11 @@ if (!isset($_SESSION['username'])) {
         }
 
         a {
-            color: #007BFF;
             text-decoration: none;
+        }
+
+        a.admin-username {
+            color: red;
         }
 
         a:hover {
@@ -73,7 +66,7 @@ if (!isset($_SESSION['username'])) {
     // You can add your code here to display the user list
     require('connect.php');
     try {
-        $select_stmt = $pdo->prepare("SELECT username, email FROM user");
+        $select_stmt = $pdo->prepare("SELECT username, email, role FROM user");
         $select_stmt->execute();
 
         if ($select_stmt->rowCount() > 0) {
@@ -86,7 +79,14 @@ if (!isset($_SESSION['username'])) {
 
             while ($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
                 echo '<tr>';
-                echo '<td><a href="profile.php?username=' . $row['username'] . '">' . $row['username'] . '</a></td>';
+                
+                // Check if the user has the "admin" role
+                $isAdmin = $row['role'] === 'admin';
+                
+                // Apply red color to admin user's username
+                $usernameStyle = $isAdmin ? 'class="admin-username"' : '';
+
+                echo '<td><a href="profile.php?username=' . $row['username'] . '" ' . $usernameStyle . '>' . $row['username'] . '</a></td>';
                 echo '<td>' . $row['email'] . '</td>';
                 echo '</tr>';
             }
